2f3ea771ae59bf779e219dd592ff5328609f553b
Mark Wang <wangyouan0629@hotmail.com>
2016-05-04 20:40:36 +0800

    Delete unused code Add class for kryo serializer

--- src/main/java/org/deeplearning4j/examples/cnn/HydraKryoSerializer.java
+++ src/main/java/org/deeplearning4j/examples/cnn/HydraKryoSerializer.java
@@ -5,16 +5,30 @@ package org.deeplearning4j.examples.cnn;
  */
 
 import com.esotericsoftware.kryo.Kryo;
+import de.javakaffee.kryoserializers.SynchronizedCollectionsSerializer;
 import org.apache.spark.serializer.KryoRegistrator;
+import org.apache.spark.util.StatCounter;
+import org.deeplearning4j.nn.api.Updater;
 import org.deeplearning4j.nn.updater.AdaGradUpdater;
 import org.deeplearning4j.nn.updater.MultiLayerUpdater;
+import org.deeplearning4j.nn.updater.NesterovsUpdater;
+import org.deeplearning4j.nn.updater.aggregate.UpdaterAggregator;
+import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.buffer.FloatBuffer;
 import org.nd4j.linalg.cpu.NDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.learning.AdaGrad;
+import org.nd4j.linalg.learning.Nesterovs;
+import scala.collection.mutable.WrappedArray;
+import scala.collection.mutable.WrappedArray$;
 
 import java.io.Serializable;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * com.lordjoe.distributed.hydra.HydraKryoSerializer
@@ -41,12 +55,31 @@ public class HydraKryoSerializer implements KryoRegistrator, Serializable {
     /**
      * do the real work of registering all classes
      *
-     * @param kryo
+     * @param kryo kryo object
      */
     @Override
     public void registerClasses(Kryo kryo) {
         kryo.register(Object[].class);
         kryo.register(scala.Tuple2[].class);
+        kryo.register(LinkedHashMap.class);
+        kryo.register(NesterovsUpdater.class);
+        kryo.register(Nesterovs.class);
+        kryo.register(DataBuffer.class);
+        kryo.register(DataBuffer.AllocationMode.class);
+        kryo.register(AtomicBoolean.class);
+        kryo.register(double[].class);
+        kryo.register(float[].class);
+        kryo.register(int[].class);
+        kryo.register(boolean[].class);
+        kryo.register(StatCounter.class);
+        try{
+            kryo.register(Class.forName("org.deeplearning4j.nn.updater.MultiLayerUpdater$MultiLayerUpdaterAggregator"));
+            kryo.register(Class.forName("org.deeplearning4j.nn.updater.NesterovsUpdater$NesterovsAggregator"));
+            kryo.register(Class.forName("org.nd4j.linalg.learning.Nesterovs$NesterovsAggregator"));
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        SynchronizedCollectionsSerializer.registerSerializers(kryo);
 
         doRegistration(kryo, FloatBuffer.class);
         doRegistration(kryo, NDArray.class);
@@ -54,6 +87,11 @@ public class HydraKryoSerializer implements KryoRegistrator, Serializable {
         doRegistration(kryo, DataSet.class);
         doRegistration(kryo, AdaGradUpdater.class);
         doRegistration(kryo, MultiLayerUpdater.class);
+        doRegistration(kryo, Updater.class);
+        doRegistration(kryo, HashSet.class);
+        doRegistration(kryo, WrappedArray.ofRef.class);
+        doRegistration(kryo, ArrayList.class);
+        doRegistration(kryo, UpdaterAggregator.class);
     }
 }
 
