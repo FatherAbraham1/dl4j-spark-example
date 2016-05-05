9efdc38f003ea9d88af1e41fe20cec53bf9faad4
Mark Wang <wangyouan0629@hotmail.com>
2016-05-05 12:15:47 +0800

    Update log settings

--- start-application.sh
+++ start-application.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-spark-submit --class org.deeplearning4j.cnn.MnistExample --master local[4] ./target/dl4j-spark-cdh5-examples-1.0-SNAPSHOT.jar
+spark-submit --class org.deeplearning4j.cnn.MnistExample --master spark://student32-x1:7077 ./target/dl4j-spark-cdh5-examples-1.0-SNAPSHOT.jar
